df2f7feffa83a97d382b1b6338fbc02eade62ae9
owokosteve <stephenowoko2@gmail.com>
2023-10-16 13:54:00 +0300

    Initial grid html and css setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,174 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Sudoku</h1>
       </header>
+      <main>
+        <div className='grid container-xl'>
+          <div className='row'>
+            <div className='region col'>
+              <div className='row'>
+                <div className='cell col-4'>1</div>
+                <div className='cell col-4'>2</div>
+                <div className='cell col-4'>3</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>4</div>
+                <div className='cell col-4'>5</div>
+                <div className='cell col-4'>6</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>7</div>
+                <div className='cell col-4'>8</div>
+                <div className='cell col-4'>9</div>
+              </div>
+            </div>
+            <div className='region col'>
+              <div className='row'>
+                <div className='cell col-4'>1</div>
+                <div className='cell col-4'>2</div>
+                <div className='cell col-4'>3</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>4</div>
+                <div className='cell col-4'>5</div>
+                <div className='cell col-4'>6</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>7</div>
+                <div className='cell col-4'>8</div>
+                <div className='cell col-4'>9</div>
+              </div>
+            </div>
+            <div className='region col'>
+              <div className='row'>
+                <div className='cell col-4'>1</div>
+                <div className='cell col-4'>2</div>
+                <div className='cell col-4'>3</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>4</div>
+                <div className='cell col-4'>5</div>
+                <div className='cell col-4'>6</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>7</div>
+                <div className='cell col-4'>8</div>
+                <div className='cell col-4'>9</div>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='region col'>
+              <div className='row'>
+                <div className='cell col-4'>1</div>
+                <div className='cell col-4'>2</div>
+                <div className='cell col-4'>3</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>4</div>
+                <div className='cell col-4'>5</div>
+                <div className='cell col-4'>6</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>7</div>
+                <div className='cell col-4'>8</div>
+                <div className='cell col-4'>9</div>
+              </div>
+            </div>
+            <div className='region col'>
+              <div className='row'>
+                <div className='cell col-4'>1</div>
+                <div className='cell col-4'>2</div>
+                <div className='cell col-4'>3</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>4</div>
+                <div className='cell col-4'>5</div>
+                <div className='cell col-4'>6</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>7</div>
+                <div className='cell col-4'>8</div>
+                <div className='cell col-4'>9</div>
+              </div>
+            </div>
+            <div className='region col'>
+              <div className='row'>
+                <div className='cell col-4'>1</div>
+                <div className='cell col-4'>2</div>
+                <div className='cell col-4'>3</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>4</div>
+                <div className='cell col-4'>5</div>
+                <div className='cell col-4'>6</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>7</div>
+                <div className='cell col-4'>8</div>
+                <div className='cell col-4'>9</div>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='region col'>
+              <div className='row'>
+                <div className='cell col-4'>1</div>
+                <div className='cell col-4'>2</div>
+                <div className='cell col-4'>3</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>4</div>
+                <div className='cell col-4'>5</div>
+                <div className='cell col-4'>6</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>7</div>
+                <div className='cell col-4'>8</div>
+                <div className='cell col-4'>9</div>
+              </div>
+            </div>
+            <div className='region col'>
+              <div className='row'>
+                <div className='cell col-4'>1</div>
+                <div className='cell col-4'>2</div>
+                <div className='cell col-4'>3</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>4</div>
+                <div className='cell col-4'>5</div>
+                <div className='cell col-4'>6</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>7</div>
+                <div className='cell col-4'>8</div>
+                <div className='cell col-4'>9</div>
+              </div>
+            </div>
+            <div className='region col'>
+              <div className='row'>
+                <div className='cell col-4'>1</div>
+                <div className='cell col-4'>2</div>
+                <div className='cell col-4'>3</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>4</div>
+                <div className='cell col-4'>5</div>
+                <div className='cell col-4'>6</div>
+              </div>
+              <div className='row'>
+                <div className='cell col-4'>7</div>
+                <div className='cell col-4'>8</div>
+                <div className='cell col-4'>9</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
